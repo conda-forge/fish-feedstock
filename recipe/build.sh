f8f4ab63dc8964becf88249b02b20af8a6556626
Chris Burr <christopher.burr@cern.ch>
2021-03-01 16:58:15 +0100

    Try to fix macOS arm

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,8 @@ cmake ${CMAKE_ARGS} .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DGETTEXT_MSGFMT_EXECUTABLE="${BUILD_PREFIX}/bin/msgfmt" \
-    -DFISH_USE_SYSTEM_PCRE2=ON
+    -DFISH_USE_SYSTEM_PCRE2=ON \
+    -Dmbrtowc_invalid_utf8_exit=OFF
 
 make -j$CPU_COUNT
 
