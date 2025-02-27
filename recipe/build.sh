f210bfcdf134630975d32123500fd643ac205b39
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2025-02-27 17:18:46 +0100

    Fish 4.0.0 and rattler-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,7 @@ cd build
 cmake ${CMAKE_ARGS} .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DGETTEXT_MSGFMT_EXECUTABLE="${BUILD_PREFIX}/bin/msgfmt" \
     -DFISH_USE_SYSTEM_PCRE2=ON \
     -Dmbrtowc_invalid_utf8_exit=OFF
-
-make -j$CPU_COUNT
-
-make install
+cmake --build .
+cmake --install .
