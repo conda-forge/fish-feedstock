3ab6e599fcdff06b2a579eff4cc769d065e6be32
Andre Anjos <andre.dos.anjos@gmail.com>
2025-04-26 18:12:34 +0200

    build: clean-up and segment by platform

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,38 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -exo pipefail
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 mkdir build
 cd build
+
 export RUSTFLAGS="${CARGO_BUILD_RUSTFLAGS}"
-if [[ "${target_platform}" == "linux-ppc64le" ]]; then
-  # CFLAGS -mcpu=power8 -mtune=power8 break the ppc64le build
-  export CFLAGS=$(echo $CFLAGS | sed -E 's?-mcpu=[^ ]+ ??; s?-mtune=[^ ]+ ??')
+
+extra_cmake_args=(
+    -DWITH_GETTEXT=ON
+    -DGETTEXT_MSGFMT_EXECUTABLE="${BUILD_PREFIX}/bin/msgfmt"
+    -DFISH_USE_SYSTEM_PCRE2=ON
+)
+
+case "${target_platform}" in
+linux-ppc64le)
+    # CFLAGS -mcpu=power8 -mtune=power8 break the ppc64le build
+    export CFLAGS=$(echo $CFLAGS | sed -E 's?-mcpu=[^ ]+ ??; s?-mtune=[^ ]+ ??')
+    ;;
+osx-64 | osx-arm64)
+    extra_cmake_args+=(-DMAC_CODESIGN_ID=OFF)
+    ;;
+esac
+
+if [[ "${target_platform}" != "${build_platform}" ]]; then
+    extra_cmake_args+=(
+        -DRUST_BUILD_TARGET=${CARGO_BUILD_TARGET}
+        -DRust_CARGO_TARGET=${CARGO_BUILD_TARGET}
+    )
 fi
-cmake ${CMAKE_ARGS} .. \
-      -DCMAKE_BUILD_TYPE="Release" \
-      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-      -DRUST_BUILD_TARGET=$CARGO_BUILD_TARGET \
-      -DRust_CARGO_TARGET=${CARGO_BUILD_TARGET} \
-      -DWITH_GETTEXT=ON \
-      -DGETTEXT_MSGFMT_EXECUTABLE="${BUILD_PREFIX}/bin/msgfmt" \
-      -DFISH_USE_SYSTEM_PCRE2=ON \
-      -DMAC_CODESIGN_ID=OFF
+
+cmake ${CMAKE_ARGS} "${extra_cmake_args}" ${SRC_DIR}
 
 cmake --build .
 cmake --install .
